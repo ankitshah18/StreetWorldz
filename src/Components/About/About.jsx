c880937aec4ef2b0fdacd72ba8ee5a86b4f4fce8
Ankit Shah <ankitshahankit181@gmail.com>
2024-07-15 11:14:21 +0530

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./About.css";
+import about_img from "../../assets/about.png";
+import play_icon from "../../assets/play-icon.png";
+
+const About = ({ setPlayState }) => {
+  return (
+    <>
+      <div className="about container">
+        <div className="about-right">
+          <div className="line-heading">
+            <div className="dark-line"></div>
+            <div className="about-heading">
+              <h2>About Us</h2>
+              <h3>Who we are?</h3>
+            </div>
+          </div>
+
+          <p>
+            At Dog Home Foundation, we are dedicated to providing a lifeline for
+            ill, injured, and accidental animals in need. Our organization
+            specializes in rescuing, treating, and sheltering these vulnerable
+            animals, giving them a second chance at a happy and healthy life.
+          </p>
+          <button className="btn-small">About Us</button>
+        </div>
+        <div className="about-left">
+          <iframe
+            src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+            frameborder="0"
+            className="about-img"
+            // height="auto"
+          ></iframe>
+          {/* <img src={about_img} alt=""  /> */}
+        </div>
+      </div>
+      <div className="about-bottom container">
+        <div className="box">
+          <h3>63 K+</h3>
+          <span>Made Happy Animal</span>
+        </div>
+        <div className="box">
+          <h3>500 +</h3>
+          <span>Permanent Resident</span>
+        </div>
+        <div className="box">
+          <h3>180 +</h3>
+          <span>Successful Adoption</span>
+        </div>
+        <div className="box">
+          <h3>100 M+</h3>
+          <span>Donation Completed</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Hero.css";
+import dark_arrow from "../../assets/dark-arrow.png";
+import { FaRegHeart } from "react-icons/fa";
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <p>Help, Donate & Fundraise</p>
+        <h1>Your Contribution Is Important</h1>
+        <button className="btn">
+          <FaRegHeart />
+          Donate Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
